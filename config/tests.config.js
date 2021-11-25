b3f76d265f8af2fbaa1bfa628ffce501194498ff
Manuel Mendoza <manuel_mm97@hotmail.com>
2021-11-24 23:34:44 -0600

    feat: update webpack

--- config/tests.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { configure } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-
-configure({ throwSuggestions: true });
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <React.StrictMode>
+        <div>run storybook</div>
+    </React.StrictMode>
+);
+
+export default App;
