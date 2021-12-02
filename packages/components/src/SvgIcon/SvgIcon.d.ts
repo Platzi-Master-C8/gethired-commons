a51cf97d1e02b844415abe2b97af334504ac1844
Kevin Farid <kpelaez75@gmail.com>
2021-12-01 23:59:48 -0500

    fix: delete SvgIcon and runtime prop in babel config

--- packages/components/src/SvgIcon/SvgIcon.d.ts
+++
@@ -1,7 +0,0 @@
-import { HTMLProps } from 'react';
-
-export interface SvgIconProps extends HTMLProps<SVGAElement> {
-  children: JSX.Element;
-}
-
-export default function SvgIcon(props: SvgIconProps): JSX.Element;
