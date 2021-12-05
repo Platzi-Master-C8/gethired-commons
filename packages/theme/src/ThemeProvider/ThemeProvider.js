8d7919728bd8042ca4690e02e547ec1a1eb926e8
Kevin Farid <kpelaez75@gmail.com>
2021-12-05 16:42:59 -0500

    fix: ThemeProvider doesn't use Default Theme

--- packages/theme/src/ThemeProvider/ThemeProvider.js
+++ packages/theme/src/ThemeProvider/ThemeProvider.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { createTheme , ThemeProvider as ThemeProviderMUI } from '@mui/material';
+import { createTheme, ThemeProvider as ThemeProviderMUI } from '@mui/material';
 
-import { DEFAULT_THEME } from '../theme';
+import { THEME } from '../theme';
 
 const ThemeProvider = ({ children }) => {
-  const THEME = createTheme(DEFAULT_THEME);
-  return <ThemeProviderMUI theme={THEME}>
-    {children}
-  </ThemeProviderMUI>;
+  const DEFAULT_THEME = createTheme(THEME);
+  return <ThemeProviderMUI theme={DEFAULT_THEME}>{children}</ThemeProviderMUI>;
 };
 
 ThemeProvider.propTypes = {
--- packages/theme/src/ThemeProvider/Themeprovider.d.ts
+++ packages/theme/src/ThemeProvider/Themeprovider.d.ts
@@ -2,7 +2,6 @@ import { ReactNode, ReactElement } from 'react';
 
 export interface ThemeProviderProps {
   children?: ReactNode;
-  theme?: any;
 }
 
-export default function ThemeProvider(props: ThemeProviderProps): ReactElement<ThemeProviderProps>;
+export default function ThemeProvider(props: ThemeProviderProps): ReactElement;
